60085eb13b7c4a9406d3710357f383b4346334ae
NinaSleephead <kx1214@gmail.com>
2016-08-24 22:48:23 -0400

    XML-AJAX-Practice
    Practice for parsing XML using AJAX

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+var httpRequest; 
+if(window.XMLHttpRequest) {
+	httpRequest = new XMLHttpRequest();
+} else {
+	httpRequest = new ActiveXObject('Microsoft.XMLHTTP');
+}
+
+httpRequest.open('GET', 'data/data.xml');
+
+httpRequest.onreadystatechange = function(){
+	if((httpRequest.status == 200)&&(httpRequest.readyState==4)) {
+	    var data = httpRequest.responseXML.getElementsByTagName('shortname');
+	    var result = '<ul>';
+	    for (var i = 0; i < data.length; i++) {
+	        result += '<li>' + data[i].firstChild.nodeValue + '</li>';
+	    }
+	    result = '</ul>';
+	    document.getElementById('readData').innerHTML = result;
+	}
+}
+httpRequest.send();
+
+
